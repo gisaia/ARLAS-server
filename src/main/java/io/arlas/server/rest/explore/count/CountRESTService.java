414e2837251c822e6108e8ffbee4504d1255cc2b
Gaudan <sylvain.gaudan@gisaia.com>
2017-04-05 18:24:04 +0200

    cout api

--- src/main/java/io/arlas/server/rest/explore/count/CountRESTService.java
+++ src/main/java/io/arlas/server/rest/explore/count/CountRESTService.java
@@ -14,20 +14,88 @@ import java.util.concurrent.ExecutionException;
 public class CountRESTService extends ExploreServices {
 
     @Timed
-    @Path("{collections}/count") // TODO : fill it
+    @Path("{collections}/count")
     @GET
     @Produces(UTF8JSON)
     @Consumes(UTF8JSON)
     @ApiOperation(
-            value="...", // TODO : fill it
+            value="Count",
             produces=UTF8JSON,
-            notes = "...", // TODO : fill it
+            notes = "Count the number of elements found in the collection(s), given the filters",
             consumes=UTF8JSON
     )
-    public Response tile(
-            @ApiParam(value="collections", required=true) // TODO : fill it
-            @PathParam(value = "collections") String collections, // TODO : fill it
+    public Response count(
+            // --------------------------------------------------------
+            // -----------------------  PATH    -----------------------
+            // --------------------------------------------------------
+            @ApiParam(
+                    name = "collections",
+                    value="collections, comma separated",
+                    allowMultiple = false,
+                    required=true)
+            @PathParam(value = "collections") String collections,
 
+            // --------------------------------------------------------
+            // -----------------------  FILTER  -----------------------
+            // --------------------------------------------------------
+            @ApiParam(name ="f",
+                    value="A triplet for filtering the result. Multiple filter can be provided. " +
+                    "The order does not matter. A triplet is composed of a field name, a comparison operator and a value. " +
+                    "The AND operator is applied between filters having different fieldNames. " +
+                    "The OR operator is applied on filters having the same fieldName. " +
+                    "If the fieldName starts with - then a must not filter is used",
+                    allowMultiple = true,
+                    required=false)
+            @QueryParam(value="f") String f,
+
+            @ApiParam(name ="q", value="A full text search",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="q") String q,
+
+            @ApiParam(name ="before", value="Any element having its point in time reference before the given timestamp",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="before") Long before,
+
+            @ApiParam(name ="after", value="Any element having its point in time reference after the given timestamp",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="after") Long after,
+
+            @ApiParam(name ="pwithin", value="Any element having its centroid contained within the given geometry (WKT)",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="pwithin") String pwithin,
+
+            @ApiParam(name ="gwithin", value="Any element having its geometry contained within the given geometry (WKT)",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="gwithin") String gwithin,
+
+            @ApiParam(name ="gintersect", value="Any element having its geometry intersecting the given geometry (WKT)",
+                    allowMultiple = false,
+                    required=false)
+            @QueryParam(value="gintersect") String gintersect,
+
+            // --------------------------------------------------------
+            // -----------------------  FORM    -----------------------
+            // --------------------------------------------------------
+            @ApiParam(name ="pretty", value="Pretty print",
+                    allowMultiple = false,
+                    defaultValue = "false",
+                    required=false)
+            @QueryParam(value="pretty") Boolean pretty,
+
+            @ApiParam(name ="human", value="Human readable print",
+                    allowMultiple = false,
+                    defaultValue = "false",
+                    required=false)
+            @QueryParam(value="human") Boolean human,
+
+            // --------------------------------------------------------
+            // -----------------------  EXTRA   -----------------------
+            // --------------------------------------------------------
             @ApiParam(value="max-age-cache", required=false)
             @QueryParam(value="max-age-cache") Integer maxagecache
     ) throws InterruptedException, ExecutionException, IOException {
