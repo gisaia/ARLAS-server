d0f3e388103180350ea631edbe6227e3acd9bd5c
sebastien.falquier <sebastien.falquier@gisaia.com>
2017-07-21 14:06:04 +0200

    #72 : add a check on configuration when server starts

--- src/main/java/io/arlas/server/app/ArlasServerConfiguration.java
+++ src/main/java/io/arlas/server/app/ArlasServerConfiguration.java
@@ -2,9 +2,10 @@ package io.arlas.server.app;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smoketurner.dropwizard.zipkin.ZipkinFactory;
-
+import io.arlas.server.exceptions.ArlasConfigurationException;
 import io.dropwizard.Configuration;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.elasticsearch.common.Strings;
 
 public class ArlasServerConfiguration extends Configuration {
     @JsonProperty("zipkin")
@@ -36,4 +37,33 @@ public class ArlasServerConfiguration extends Configuration {
     
     @JsonProperty("collection-auto-discover")
     public CollectionAutoDiscoverConfiguration collectionAutoDiscoverConfiguration;
+
+    public void check() throws ArlasConfigurationException {
+        if(Strings.isNullOrEmpty(elastichost) || elasticport<1
+                || (!elastichost.equalsIgnoreCase("localhost") && Strings.isNullOrEmpty(elasticcluster))) {
+            throw new ArlasConfigurationException("Elastic search configuration missing in config file.");
+        }
+        if(zipkinConfiguration == null) {
+            throw new ArlasConfigurationException("Zipkin configuration missing in config file.");
+        }
+        if(swaggerBundleConfiguration == null) {
+            throw new ArlasConfigurationException("Swagger configuration missing in config file.");
+        }
+        if(Strings.isNullOrEmpty(arlasindex)) {
+            arlasindex = ".arlas";
+        }
+        if(arlascachesize<0) {
+            arlascachesize = 1000;
+        }
+        if(arlascachetimeout<0) {
+            arlascachetimeout = 60;
+        }
+        if(arlascorsenabled==null) {
+            arlascorsenabled = false;
+        }
+        if(collectionAutoDiscoverConfiguration == null) {
+            collectionAutoDiscoverConfiguration = new CollectionAutoDiscoverConfiguration();
+            collectionAutoDiscoverConfiguration.schedule = 0;
+        }
+    }
 }
---
+++ src/main/java/io/arlas/server/exceptions/ArlasConfigurationException.java
@@ -0,0 +1,6 @@
+package io.arlas.server.exceptions;
+
+public class ArlasConfigurationException extends ArlasException {
+    private static final long serialVersionUID = 1L;
+    public ArlasConfigurationException(String message) { super(message); }
+}
