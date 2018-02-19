4fcbf513f04b8aab939d94da4d4dc4f448194f04
sebastien.falquier <sebastien.falquier@gisaia.com>
2018-02-19 21:02:58 +0100

    release version 2.5.5

--- src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+++ src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
@@ -36,7 +36,7 @@ import javax.ws.rs.core.Response;
                 title = "ARLAS Exploration API",
                 description = "Explore the content of ARLAS collections",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "API_VERSION"))
+                version = "2.0.5"))
 
 public abstract class ExploreRESTServices {
 
