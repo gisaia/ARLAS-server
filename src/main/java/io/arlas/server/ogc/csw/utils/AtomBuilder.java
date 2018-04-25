e9650268294de532378b0e6a08aa302a1f182994
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2018-04-25 16:29:03 +0200

    Fix full atom csw getrecord response

--- src/main/java/io/arlas/server/ogc/csw/utils/AtomBuilder.java
+++ src/main/java/io/arlas/server/ogc/csw/utils/AtomBuilder.java
@@ -24,8 +24,12 @@ import net.opengis.cat.csw._3.AbstractRecordType;
 import net.opengis.cat.csw._3.BriefRecordType;
 import net.opengis.cat.csw._3.RecordType;
 import net.opengis.cat.csw._3.SummaryRecordType;
+import org.purl.dc.elements._1.SimpleLiteral;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3._2005.atom.*;
 
+import javax.ws.rs.WebApplicationException;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -34,48 +38,103 @@ import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 public class AtomBuilder {
+    public static Logger LOGGER = LoggerFactory.getLogger(AtomBuilder.class);
 
-    public static void setEntryType(AbstractRecordType abstractRecordType, FeedType feedType, EntryType entryType){
+    public static void setEntryType(AbstractRecordType abstractRecordType, FeedType feedType, EntryType entryType) {
         String recordType = abstractRecordType.getClass().getSimpleName();
-        switch (recordType){
+        switch (recordType) {
             case "BriefRecordType":
-                setBriefRecordType((BriefRecordType)abstractRecordType,entryType);
+                setBriefRecordType((BriefRecordType) abstractRecordType, entryType);
                 break;
             case "SummaryRecordType":
-                setSummaryRecordType((SummaryRecordType)abstractRecordType,entryType);
+                setSummaryRecordType((SummaryRecordType) abstractRecordType, entryType);
                 break;
             case "RecordType":
-                setRecordType((RecordType)abstractRecordType,entryType);
+                setRecordType((RecordType) abstractRecordType, entryType);
                 break;
         }
     }
 
     public static void setRecordType(RecordType recordType, EntryType entryType) {
+        org.purl.dc.elements._1.ObjectFactory objectFactory = new org.purl.dc.elements._1.ObjectFactory();
         recordType.getDCElement().stream().forEach(simpleLiteralJAXBElement -> {
-            entryType.getOtherAttributes().put(
-                    simpleLiteralJAXBElement.getName(),simpleLiteralJAXBElement.getValue().toString());
+            LOGGER.info(simpleLiteralJAXBElement.getName().getLocalPart());
+            String value = simpleLiteralJAXBElement.getValue().getContent().get(0);
+            TextType textType = new TextType();
+            textType.getContent().add(value);
+            SimpleLiteral simpleLiteral = objectFactory.createSimpleLiteral();
+            simpleLiteral.getContent().add(value);
+            switch (simpleLiteralJAXBElement.getName().getLocalPart()) {
+                case "title":
+                    entryType.setTitle(textType);
+                    break;
+                case "subject":
+                    entryType.setSubject(simpleLiteral);
+                    break;
+                case "type":
+                    entryType.setType(simpleLiteral);
+                    break;
+                case "date":
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                    try {
+                        GregorianCalendar c = new GregorianCalendar();
+                        c.setTime(simpleDateFormat.parse(value));
+                        try {
+                            XMLGregorianCalendar date = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+                            DateTimeType dateTimeType = new DateTimeType();
+                            dateTimeType.setValue(date);
+                            entryType.setPublished(dateTimeType);
+                            entryType.setUpdated(dateTimeType);
+                        } catch (DatatypeConfigurationException e) {
+                            e.printStackTrace();
+                            new WebApplicationException(e);
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        new WebApplicationException(e);
+
+                    }
+                    break;
+                case "format":
+                    entryType.setFormat(objectFactory.createFormat(simpleLiteral));
+                    break;
+                case "description":
+                    entryType.setSummary(textType);
+                    break;
+                case "identifier":
+                    entryType.setIdentifier(objectFactory.createIdentifier(simpleLiteral));
+                    break;
+            }
         });
+        String south = recordType.getBoundingBox().get(0).getValue().getLowerCorner().get(0);
+        String west = recordType.getBoundingBox().get(0).getValue().getLowerCorner().get(1);
+        String north = recordType.getBoundingBox().get(0).getValue().getUpperCorner().get(0);
+        String east = recordType.getBoundingBox().get(0).getValue().getUpperCorner().get(1);
+        entryType.getBox().add(south);
+        entryType.getBox().add(west);
+        entryType.getBox().add(north);
+        entryType.getBox().add(east);
     }
 
     public static void setSummaryRecordType(SummaryRecordType summaryRecordType, EntryType entryType) {
         ObjectFactory objectFactory = new ObjectFactory();
         TextType title = objectFactory.createTextType();
-        summaryRecordType.getTitle().forEach(t->{
-            t.getValue().getContent().forEach(o->{
+        summaryRecordType.getTitle().forEach(t -> {
+            t.getValue().getContent().forEach(o -> {
                 title.getContent().add(o);
             });
         });
         IdType idTypeValue = new IdType();
-        summaryRecordType.getIdentifier().forEach(id->{
+        summaryRecordType.getIdentifier().forEach(id -> {
             entryType.setIdentifier(id);
-            id.getValue().getContent().forEach(content->{
+            id.getValue().getContent().forEach(content -> {
                 idTypeValue.setValue(content);
 
             });
         });
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        summaryRecordType.getModified().forEach(d->{
-            d.getContent().forEach(v->{
+        summaryRecordType.getModified().forEach(d -> {
+            d.getContent().forEach(v -> {
                 try {
                     GregorianCalendar c = new GregorianCalendar();
                     c.setTime(simpleDateFormat.parse(v));
@@ -86,31 +145,49 @@ public class AtomBuilder {
                         entryType.setUpdated(dateTimeType);
                     } catch (DatatypeConfigurationException e) {
                         e.printStackTrace();
+                        new WebApplicationException(e);
                     }
                 } catch (ParseException e) {
                     e.printStackTrace();
+                    new WebApplicationException(e);
                 }
             });
         });
         entryType.setId(idTypeValue);
         entryType.setTitle(title);
+        String south = summaryRecordType.getBoundingBox().get(0).getValue().getLowerCorner().get(0);
+        String west = summaryRecordType.getBoundingBox().get(0).getValue().getLowerCorner().get(1);
+        String north = summaryRecordType.getBoundingBox().get(0).getValue().getUpperCorner().get(0);
+        String east = summaryRecordType.getBoundingBox().get(0).getValue().getUpperCorner().get(1);
+        entryType.getBox().add(south);
+        entryType.getBox().add(west);
+        entryType.getBox().add(north);
+        entryType.getBox().add(east);
     }
 
-    public static void setBriefRecordType(BriefRecordType briefRecordType,EntryType entryType) {
+    public static void setBriefRecordType(BriefRecordType briefRecordType, EntryType entryType) {
         ObjectFactory objectFactory = new ObjectFactory();
         TextType title = objectFactory.createTextType();
-        briefRecordType.getTitle().forEach(t->{
-            t.getValue().getContent().forEach(o->{
+        briefRecordType.getTitle().forEach(t -> {
+            t.getValue().getContent().forEach(o -> {
                 title.getContent().add(o);
             });
         });
         IdType idTypeValue = new IdType();
-        briefRecordType.getIdentifier().forEach(id->{
-            id.getValue().getContent().forEach(content->{
+        briefRecordType.getIdentifier().forEach(id -> {
+            id.getValue().getContent().forEach(content -> {
                 idTypeValue.setValue(content);
             });
         });
         entryType.setId(idTypeValue);
         entryType.setTitle(title);
+        String south = briefRecordType.getBoundingBox().get(0).getValue().getLowerCorner().get(0);
+        String west = briefRecordType.getBoundingBox().get(0).getValue().getLowerCorner().get(1);
+        String north = briefRecordType.getBoundingBox().get(0).getValue().getUpperCorner().get(0);
+        String east = briefRecordType.getBoundingBox().get(0).getValue().getUpperCorner().get(1);
+        entryType.getBox().add(south);
+        entryType.getBox().add(west);
+        entryType.getBox().add(north);
+        entryType.getBox().add(east);
     }
 }
