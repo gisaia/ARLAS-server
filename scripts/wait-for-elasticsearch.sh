c297853c9331f7a28e84308959faeacd7b38d2d1
elouanKeryell-Even <elouan.keryell@gmail.com>
2018-07-03 18:03:57 +0200

    Made `scripts/wait-for-elasticsearch.sh` support environment variable `ARLAS_ELASTIC_NODES`.

--- scripts/wait-for-elasticsearch.sh
+++ scripts/wait-for-elasticsearch.sh
@@ -1,8 +1,33 @@
-#!/bin/sh
+#!/bin/sh -ex
 set -e
 
-host="$1"
-port="$2"
+script_name="$(basename "$(readlink -f "$0")")"
+
+if [[ $# == 2 ]]; then
+  host="$1"
+  port="$2"
+elif ! [[ -z ${ARLAS_ELASTIC_NODES+x} ]] && [[ $# == 0 ]]; then
+  # No arguments are passed, & ARLAS_ELASTIC_NODES is set
+  # Will use ARLAS_ELASTIC_NODES
+  first_elasticsearch_node=${ARLAS_ELASTIC_NODES%%,*}
+  host=${first_elasticsearch_node%:*}
+
+  case "$first_elasticsearch_node" in
+    *:*)
+      # port specified, extracting it
+      port=${first_elasticsearch_node#*:}    
+      ;;
+    *)
+      # no port specified, using default
+      port=9300
+      ;;
+
+  esac
+
+else
+  >&2 echo "[ERROR] $script_name -- host & port not correctly passed."
+  exit 1
+fi
 
 i=1; until nc -w 2 $host $port; do if [ $i -lt 30 ]; then sleep 1; else break; fi; i=$(($i + 1)); echo "try to connect to $host:$port"; done
 
