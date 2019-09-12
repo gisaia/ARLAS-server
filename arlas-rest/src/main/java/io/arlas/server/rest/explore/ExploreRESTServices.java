49f81b9248f108307764cd69c557ed25ca64a252
Alain Bodiguel <alain.bodiguel@gisaia.com>
2019-09-12 09:50:14 +0200

    Add HTTPS in the swagger definition #513

--- arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+++ arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
@@ -36,7 +36,8 @@ import javax.ws.rs.core.Response;
                 title = "ARLAS Exploration API",
                 description = "Explore the content of ARLAS collections",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "API_VERSION"))
+                version = "API_VERSION"),
+        schemes = { SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS })
 
 public abstract class ExploreRESTServices {
 
