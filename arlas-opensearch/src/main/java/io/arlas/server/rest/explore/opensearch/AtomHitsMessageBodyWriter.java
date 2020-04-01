4b7e9c23074e2ba6c6dbbad1e9df1394f95516ee
alainbodiguel <alain.bodiguel@gisaia.com>
2020-04-01 15:52:57 +0200

    Refactoring packages in order to centralise ES implementation and code cleanup

--- arlas-opensearch/src/main/java/io/arlas/server/rest/explore/opensearch/AtomHitsMessageBodyWriter.java
+++ arlas-opensearch/src/main/java/io/arlas/server/rest/explore/opensearch/AtomHitsMessageBodyWriter.java
@@ -20,6 +20,7 @@
 package io.arlas.server.rest.explore.opensearch;
 
 import io.arlas.server.exceptions.ArlasException;
+import io.arlas.server.impl.elastic.core.ElasticAdmin;
 import io.arlas.server.model.CollectionReference;
 import io.arlas.server.model.Feed;
 import io.arlas.server.model.response.*;
