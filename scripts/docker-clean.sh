a59c824a91aa42f992988e18e458466e92c70f5b
MohamedHamouGisaia <mohamed.hamou@gisaia.com>
2022-05-05 12:15:20 +0200

    log es version

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -8,4 +8,5 @@ function clean_exit {
 trap clean_exit EXIT
 
 echo "===> stop arlas-server stack"
+echo $(docker ps)
 docker-compose -f docker-compose.yml -f docker-compose-elasticsearch.yml --project-name arlas down -v
