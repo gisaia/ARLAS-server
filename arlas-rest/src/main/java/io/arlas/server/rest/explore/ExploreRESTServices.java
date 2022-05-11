14e21a809c52852bd575a0e525634040f0702566
alainbodiguel <alain.bodiguel@gisaia.com>
2022-05-11 09:55:20 +0000

    development version 22.0.1-SNAPSHOT

--- arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+++ arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
@@ -63,7 +63,7 @@ import java.util.zip.ZipOutputStream;
                 title = "ARLAS Exploration API",
                 description = "Explore the content of ARLAS collections",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "22.0.0-beta.4"),
+                version = "API_VERSION"),
         schemes = { SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS })
 
 public abstract class ExploreRESTServices {
