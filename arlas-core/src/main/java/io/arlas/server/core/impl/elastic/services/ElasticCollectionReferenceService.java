5f58fe12bb5ffde12558af02ec16169a87a2a679
alainbodiguel <alain.bodiguel@gisaia.com>
2022-06-23 16:57:06 +0200

    ES8

--- arlas-core/src/main/java/io/arlas/server/core/impl/elastic/services/ElasticCollectionReferenceService.java
+++ arlas-core/src/main/java/io/arlas/server/core/impl/elastic/services/ElasticCollectionReferenceService.java
@@ -19,55 +19,35 @@
 
 package io.arlas.server.core.impl.elastic.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import io.arlas.server.core.services.CollectionReferenceService;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.mapping.Property;
+import co.elastic.clients.elasticsearch.core.DeleteResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import io.arlas.commons.exceptions.ArlasException;
 import io.arlas.commons.exceptions.InternalServerErrorException;
 import io.arlas.commons.exceptions.NotFoundException;
 import io.arlas.server.core.impl.elastic.utils.ElasticClient;
-import io.arlas.server.core.impl.elastic.utils.ElasticTool;
 import io.arlas.server.core.managers.CacheManager;
 import io.arlas.server.core.model.CollectionReference;
 import io.arlas.server.core.model.CollectionReferenceParameters;
+import io.arlas.server.core.services.CollectionReferenceService;
+import io.arlas.server.core.utils.CollectionUtil;
 import io.arlas.server.core.utils.ColumnFilterUtil;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.ClearScrollRequest;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchScrollRequest;
-import org.elasticsearch.core.TimeValue;
-import org.elasticsearch.index.IndexNotFoundException;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.sort.FieldSortBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@SuppressWarnings({"unchecked", "rawtypes"})
+import static io.arlas.server.core.model.CollectionReference.INCLUDE_FIELDS;
+
 public class ElasticCollectionReferenceService extends CollectionReferenceService {
-    private static Logger LOGGER = LoggerFactory.getLogger(ElasticCollectionReferenceService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticCollectionReferenceService.class);
 
-    private static ObjectMapper mapper;
-    private static ObjectReader reader;
     private static final String ARLAS_MAPPING_FILE_NAME = "arlas.mapping.json";
-
-    static {
-        mapper = new ObjectMapper();
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-        reader = mapper.readerFor(CollectionReferenceParameters.class);
-    }
 
     private final ElasticClient client;
 
@@ -79,24 +59,24 @@ public class ElasticCollectionReferenceService extends CollectionReferenceServic
     @Override
     public void initCollectionDatabase() throws ArlasException {
         if (client.indexExists(arlasIndex)) {
-            ElasticTool.putExtendedMapping(client, arlasIndex, this.getClass().getClassLoader().getResourceAsStream(ARLAS_MAPPING_FILE_NAME));
+            client.putMapping(arlasIndex, ARLAS_MAPPING_FILE_NAME);
         } else {
-            ElasticTool.createArlasIndex(client, arlasIndex, ARLAS_MAPPING_FILE_NAME);
+            client.createIndex(arlasIndex, ARLAS_MAPPING_FILE_NAME);
         }
     }
 
     @Override
     protected CollectionReference getCollectionReferenceFromDao(String ref) throws ArlasException {
-        return ElasticTool.getCollectionReferenceFromES(client, arlasIndex, reader, ref);
+        return client.getCollectionReferenceFromES(arlasIndex, ref);
     }
 
     @Override
-    protected Map<String, LinkedHashMap> getMappingFromDao(String indexName) throws ArlasException {
+    protected Map<String, Map<String, Property>> getMappingFromDao(String indexName) throws ArlasException {
         return client.getMappings(indexName);
     }
 
     @Override
-    protected Map<String, LinkedHashMap> getAllMappingsFromDao(String arlasIndex) throws ArlasException {
+    protected Map<String, Map<String, Property>> getAllMappingsFromDao(String arlasIndex) throws ArlasException {
         return client.getMappings();
     }
 
@@ -105,42 +85,35 @@ public class ElasticCollectionReferenceService extends CollectionReferenceServic
         List<CollectionReference> collections = new ArrayList<>();
 
         try {
-            QueryBuilder qb = QueryBuilders.matchAllQuery();
-            SearchRequest request = new SearchRequest(arlasIndex);
-            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            //Exclude old include_fields for support old collection
-            searchSourceBuilder.fetchSource(null, "include_fields")
-                    .sort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
-                    .query(qb).size(100);// max of 100 hits will be returned for each scroll
-            request.source(searchSourceBuilder);
-            request.scroll(new TimeValue(60000));
-            SearchResponse scrollResp = client.search(request);
+            SearchRequest request = SearchRequest.of(r -> r
+                    .index(arlasIndex)
+                    .source(b -> b.filter(c -> c.excludes(INCLUDE_FIELDS)))
+                    .sort(b -> b.field(c -> c.order(SortOrder.Asc)))
+                    .query(b -> b.matchAll(c -> c.queryName("matchAllQuery")))
+                    .size(100)
+            );
+//            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//            //Exclude old include_fields for support old collection
+//            searchSourceBuilder.fetchSource(null, "include_fields")
+//                    .sort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
+//                    .query(QueryBuilders.matchAllQuery())
+//                    .size(100);// max of 100 hits will be returned for each scroll
+//            SearchRequest request = new SearchRequest(arlasIndex);
+//            request.source(searchSourceBuilder);
+            SearchResponse<CollectionReferenceParameters> response = client.search(request, CollectionReferenceParameters.class);
             Set<String> allowedCollections = ColumnFilterUtil.getAllowedCollections(columnFilter);
-            do {
-                for (SearchHit hit : scrollResp.getHits().getHits()) {
-                    String source = hit.getSourceAsString();
-                    try {
-                        for (String c : allowedCollections) {
-                            if ((c.endsWith("*") && hit.getId().startsWith(c.substring(0, c.indexOf("*"))))
-                                    || hit.getId().equals(c)){
-                                collections.add(new CollectionReference(hit.getId(), reader.readValue(source)));
-                                break;
-                            }
-                        }
-                    } catch (IOException e) {
-                        throw new InternalServerErrorException("Can not fetch collection", e);
+
+            // TODO es8 add search after
+            for (Hit<CollectionReferenceParameters> hit : response.hits().hits()) {
+                for (String c : allowedCollections) {
+                    if (CollectionUtil.matches(c, hit.id())){
+                        collections.add(new CollectionReference(hit.id(), hit.source()));
+                        break;
                     }
                 }
-                SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollResp.getScrollId());
-                scrollRequest.scroll(new TimeValue(60000));
-                scrollResp = client.searchScroll(scrollRequest);
             }
-            while (scrollResp.getHits().getHits().length != 0); // Zero hits mark the end of the scroll and the while loop.
 
-            ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
-            clearScrollRequest.addScrollId(scrollResp.getScrollId());
-            client.clearScroll(clearScrollRequest);
-        } catch (IndexNotFoundException e) {
+        } catch (NotFoundException e) {
             throw new InternalServerErrorException("Unreachable collections", e);
         }
         return collections.stream().filter(c-> {
@@ -154,34 +127,30 @@ public class ElasticCollectionReferenceService extends CollectionReferenceServic
 
     @Override
     public void deleteCollectionReference(String ref) throws ArlasException {
-        DeleteResponse response = client.delete(arlasIndex, ref);
-        if (response.status().equals(RestStatus.NOT_FOUND)) {
-            throw new NotFoundException("collection " + ref + " not found.");
-        } else if (!response.status().equals(RestStatus.OK)) {
-            throw new InternalServerErrorException("Unable to delete collection : " + response.status().toString());
-        } else {
+        DeleteResponse response = client.deleteDocument(arlasIndex, ref);
+        // TODO es8
+//        if (response.status().equals(RestStatus.NOT_FOUND)) {
+//            throw new NotFoundException("collection " + ref + " not found.");
+//        } else if (!response.status().equals(RestStatus.OK)) {
+//            throw new InternalServerErrorException("Unable to delete collection : " + response.status().toString());
+//        } else {
             //explicit clean-up cache
             cacheManager.removeCollectionReference(ref);
-        }
+//        }
     }
 
     @Override
     protected void putCollectionReferenceWithDao(CollectionReference collectionReference) throws ArlasException {
-        IndexResponse response;
-        try {
-            response = client.index(arlasIndex, collectionReference.collectionName, mapper.writeValueAsString(collectionReference.params));
-        } catch (JsonProcessingException e) {
-            throw new InternalServerErrorException("Can not put collection " + collectionReference.collectionName, e);
-        }
-
-        if (response.status().getStatus() != RestStatus.OK.getStatus()
-                && response.status().getStatus() != RestStatus.CREATED.getStatus()) {
-            throw new InternalServerErrorException("Unable to index collection : " + response.status().toString());
-        }
+        IndexResponse response = client.index(arlasIndex, collectionReference.collectionName, collectionReference.params);
+        // TODO es8
+//        if (response.status().getStatus() != RestStatus.OK.getStatus()
+//                && response.status().getStatus() != RestStatus.CREATED.getStatus()) {
+//            throw new InternalServerErrorException("Unable to index collection : " + response.status().toString());
+//        }
     }
 
     @Override
     public boolean isDateField(String field, String index) throws ArlasException {
-        return ElasticTool.isDateField(field, client, index);
+        return client.isDateField(field, index);
     }
 }
--- arlas-rest/src/main/java/io/arlas/server/rest/explore/raw/RawRESTService.java
+++ arlas-rest/src/main/java/io/arlas/server/rest/explore/raw/RawRESTService.java
@@ -19,7 +19,9 @@
 
 package io.arlas.server.rest.explore.raw;
 
+import co.elastic.clients.json.JsonData;
 import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.arlas.server.core.app.Documentation;
 import io.arlas.commons.exceptions.ArlasException;
 import io.arlas.commons.exceptions.NotFoundException;
@@ -109,7 +111,7 @@ public class RawRESTService extends ExploreRESTServices {
                 .map(i -> i.split(","))
                 .orElse(null);
 
-        Map<String, Object> source = exploreService.getRawDoc(collectionReference, identifier, includes);
+        Map<String, JsonData> source = exploreService.getRawDoc(collectionReference, identifier, includes);
 
         if (source == null || source.isEmpty()) {
             throw new NotFoundException("Document " + identifier + " not found.");
