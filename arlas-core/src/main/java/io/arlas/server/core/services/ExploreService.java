d28f8d5de31eca96ebb1d66b2e6c3b2a7454df42
alainbodiguel <alain.bodiguel@gisaia.com>
2022-06-24 17:09:35 +0200

    Rename Hit to ArlasHit

--- arlas-core/src/main/java/io/arlas/server/core/services/ExploreService.java
+++ arlas-core/src/main/java/io/arlas/server/core/services/ExploreService.java
@@ -178,7 +178,7 @@ public abstract class ExploreService {
                 );
     }
 
-    protected Feature getFeatureFromHit(Hit arlasHit, String path, GeoJsonObject geometry) {
+    protected Feature getFeatureFromHit(ArlasHit arlasHit, String path, GeoJsonObject geometry) {
         Feature feature = new Feature();
         geometry.setCrs(null);
         // Setting geometry of geojson
