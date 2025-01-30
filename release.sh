fa1c17f66f07e896110bbb2e5bc7f1f255e2b6f1
MatthieuBarbet <matthieu.barbet@gisaia.com>
2025-01-30 15:02:36 +0100

    Feat:fix api version in release

--- release.sh
+++ release.sh
@@ -137,7 +137,7 @@ else echo "=> Skip develop checkout"; fi
 echo "=> Update project version"
 mvn clean
 mvn versions:set -DnewVersion=${ARLAS_VERSION}
-sed -i.bak 's/\"API_VERSION\"/\"'${FULL_API_VERSION}'\"/' arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+sed -i.bak 's/\"API_VERSION\"/\"'${FULL_API_VERSION}'\"/' arlas-rest/src/main/java/io/arlas/server/core/services/ArlasRESTServices.java
 sed -i.bak 's/^appVersion: .*$/appVersion: '${ARLAS_VERSION}'/' packaging/helm/arlas-server/Chart.yaml
 sed -i.bak 's/^version: .*$/version: '${ARLAS_VERSION}'/' packaging/helm/arlas-server/Chart.yaml
 
@@ -311,7 +311,7 @@ echo "=> Update project version for develop"
 mvn versions:set -DnewVersion=${ARLAS_DEV_VERSION}-SNAPSHOT
 
 echo "=> Update REST API version in JAVA source code"
-sed -i.bak 's/\"'${FULL_API_VERSION}'\"/\"API_VERSION\"/' arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+sed -i.bak 's/\"'${FULL_API_VERSION}'\"/\"API_VERSION\"/' arlas-rest/src/main/java/io/arlas/server/core/services/ArlasRESTServices.java
 
 if [ "$SIMULATE" == "NO" ]; then
     sed -i.bak 's/\"'${FULL_API_VERSION}'\"/\"'${API_DEV_VERSION}-SNAPSHOT'\"/' openapi/openapi.yaml
