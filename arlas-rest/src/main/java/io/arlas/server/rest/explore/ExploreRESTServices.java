09f47d0fbbdf6d7f0b08daa4dfbb36e0c8f92121
MohamedHamouGisaia <mohamed.hamou@gisaia.com>
2025-10-10 13:29:06 +0200

    development version 23.0.9-security.2-SNAPSHOT

--- arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
+++ arlas-rest/src/main/java/io/arlas/server/rest/explore/ExploreRESTServices.java
@@ -75,7 +75,7 @@ import io.swagger.annotations.SwaggerDefinition;
                 title = "ARLAS Exploration API",
                 description = "Explore the content of ARLAS collections",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "23.0.8-security.2"),
+                version = "API_VERSION"),
         schemes = { SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS })
 
 public abstract class ExploreRESTServices {
